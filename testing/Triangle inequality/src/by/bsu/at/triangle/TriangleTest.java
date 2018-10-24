a89f71cd54008317419bb02c2ef7adbe841326ab
Irina Vaskova <irvaskova.n@gmail.com>
2018-10-24 23:22:08 +0300

    triangle tests

--- testing/Triangle inequality/src/by/bsu/at/triangle/TriangleTest.java
+++ testing/Triangle inequality/src/by/bsu/at/triangle/TriangleTest.java
@@ -11,51 +11,51 @@ public class TriangleTest {
 
     @Test
     public void isTriangle() {
-        assertTrue(Triangle.isTriangle(4,5,6)==true);
+        assertTrue(Triangle.isTriangle(4,5,6));
     }
 
     @Test
     public void negativeNumbers() {
-        assertTrue(Triangle.isTriangle(-3,-3,-2)==false);
+        assertFalse(Triangle.isTriangle(-3,-3,-2));
     }
 
     @Test
     public void isnotTriangleDueToA() {
-        assertTrue(Triangle.isTriangle(10,4,5)==false);
+        assertFalse(Triangle.isTriangle(10,4,5));
     }
 
     @Test
     public void isnotTriangleDueToB() {
-        assertTrue(Triangle.isTriangle(4,10,5)==false);
+        assertFalse(Triangle.isTriangle(4,10,5));
     }
 
     @Test
     public void isnotTriangleDueToC() {
-        assertTrue(Triangle.isTriangle(5,4,10)==false);
+        assertFalse(Triangle.isTriangle(5,4,10));
     }
 
     @Test
-    public void zero() {
-        assertTrue(Triangle.isTriangle(0,0,0)==false);
+    public void AisZero() {
+        assertFalse(Triangle.isTriangle(0,5,3));
+    }
+
+    @Test
+    public void BisZero() {
+        assertFalse(Triangle.isTriangle(10,0,3));
+    }
+
+    @Test
+    public void CisZero() {
+        assertFalse(Triangle.isTriangle(6,5,0));
     }
 
     @Test
     public void equilateralTriangle() {
-        assertTrue(Triangle.isTriangle(5,5,5)==true);
-    }
-
-    @Test
-    public void resultIsNotNull() {
-        assertNotNull(Triangle.isTriangle(11,5,6));
+        assertTrue(Triangle.isTriangle(5,5,5));
     }
 
     @Test
     public void AplusBequalsC() {
-        assertTrue(Triangle.isTriangle(10,5,15)==false);
-    }
-
-    @Test
-    public void ifValueIsNull() {
-        assertTrue(Triangle.isTriangle(3,4,NULL)==false);
+        assertFalse(Triangle.isTriangle(10,5,15));
     }
 }
