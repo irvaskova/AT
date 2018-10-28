6250a53c4dbdc832ebf7d978b4ef080958d080d5
Irina Vaskova <irvaskova.n@gmail.com>
2018-10-28 20:56:31 +0300

    added iutesting

---
+++ testing/webdriver/src/test/java/SimpleTest.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SimpleTest {
+
+    @Test
+    public void correctFillingOfAllFields() {
+
+        System.setProperty("webdriver.chrome.driver", "D:\\chromedriver_win32\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.swiss.com/");
+        driver.findElement(By.id("FlightOrigin")).clear();
+        driver.findElement(By.id("FlightOrigin")).sendKeys("London");
+        driver.findElement(By.id("FlightDestination")).clear();
+        driver.findElement(By.id("FlightDestination")).sendKeys("Deli");
+        driver.findElement(By.className("evo-input--label js-evo-passenger_select--infolabel")).click();
+        driver.findElement(By.xpath("//div[@class='evo-input as-count']//label[@class='evo-input--label' @for='FlightAdults']//button[@class='evo-input-number--button as-increase js-stepinput--increase']")).click();
+        driver.findElement(By.className("evo-passenger_select--popup--button js-evo-passenger_select--close")).click();
+        driver.findElement(By.className("evo-button js-evo-bookingbar--submit")).click();
+    }
+
+}
